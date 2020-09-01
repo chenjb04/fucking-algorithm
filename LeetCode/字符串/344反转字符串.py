caae83ad5320d1be469b128a7a3414bd59550d61
chenjb <chenjb04@163.com>
2020-09-01 14:07:39 +0800

    字符串反转

---
+++ LeetCode/字符串/344反转字符串.py
@@ -0,0 +1,46 @@
+"""
+编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。 
+
+ 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。 
+
+ 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。 
+
+ 
+
+ 示例 1： 
+
+ 输入：["h","e","l","l","o"]
+输出：["o","l","l","e","h"]
+ 
+
+ 示例 2： 
+
+ 输入：["H","a","n","n","a","h"]
+输出：["h","a","n","n","a","H"] 
+ Related Topics 双指针 字符串
+"""
+from typing import List
+
+
+"""
+双指针： 第一个从0开始，第二个指针从最后一个元素开始， 交换双指针位置的元素，直到双指针重合
+"""
+def reverseString(s: List[str]) -> None:
+	if not s:
+		return
+	n = len(s)
+	i = 0
+	j = n - 1
+	while i <= j:
+		s[i], s[j]= s[j], s[i]
+		i += 1
+		j -= 1
+
+
+if __name__ == '__main__':
+	s = ["h","e","l","l","o"]
+	reverseString(s)
+	print(s)
+	s = ["H","a","n","n","a","h"]
+	reverseString(s)
+	print(s)
