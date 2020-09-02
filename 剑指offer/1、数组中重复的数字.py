583a5ea74d70b6c5c6aca950577a206d26320b85
chenjb <chenjb04@163.com>
2020-09-02 09:46:38 +0800

    数组中重复的数字

---
+++ 剑指offer/1、数组中重复的数字.py
@@ -0,0 +1,42 @@
+"""
+找出数组中重复的数字。 
+
+
+在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请
+找出数组中任意一个重复的数字。 
+
+示例 1： 
+
+输入：
+[2, 3, 1, 0, 2, 5, 3]
+输出：2 或 3 
+
+
+
+
+限制： 
+
+2 <= n <= 100000 
+Related Topics 数组 哈希表
+"""
+from typing import List
+
+
+def findRepeatNumber(nums: List[int]) -> int:
+	if not nums:
+		return 0
+	n = len(nums)
+	hash_map = {}
+	for i in range(n):
+		if nums[i] not in hash_map:
+			hash_map[nums[i]] = 0
+		else:
+			hash_map[nums[i]] += 1
+	return [k for k, v in hash_map.items() if v > 0][0]
+
+
+if __name__ == '__main__':
+	nums = [2, 3, 1, 0, 2, 5, 3]
+	print(findRepeatNumber(nums))
+	nums = []
+	print(findRepeatNumber(nums))
