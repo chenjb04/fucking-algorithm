f3843d5224c6611877073b7fdf07eee2d9a62a14
chenjb <chenjb04@163.com>
2020-09-29 11:22:13 +0800

    N叉树后序遍历

--- LeetCode/动态规划/121买卖股票的最佳时机.py
+++ LeetCode/动态规划/121买卖股票的最佳时机.py
@@ -5,14 +5,14 @@ from typing import List
 最大利润=max{前一天最大利润, 今天的价格 - 之前最低价格}
 """
 def maxProfit(prices: List[int]) -> int:
-	if not prices:
-		return 0
-	dp = [0] * (len(prices))
-	min_price = prices[0]
-	for i in range(1, len(prices)):
-		min_price = min(min_price, prices[i])
-		dp[i] = max(dp[i-1], prices[i] - min_price)
-	return dp[-1]
+		if not prices:
+			return 0
+		dp = [0] * (len(prices))
+		min_price = prices[0]
+		for i in range(1, len(prices)):
+			min_price = min(min_price, prices[i])
+			dp[i] = max(dp[i-1], prices[i] - min_price)
+		return dp[-1]
 
 
 if __name__ == '__main__':
